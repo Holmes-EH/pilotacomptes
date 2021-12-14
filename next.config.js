3e6a0839ed30965adeb7b97d3abad58a902bd0ca
Holmes-EH <holmes.samuel@yahoo.fr>
2021-12-14 17:11:21 +0100

    add support for svg as components

--- next.config.js
+++ next.config.js
@@ -8,4 +8,12 @@ module.exports = withPWA({
 		skipWaiting: true,
 		disable: process.env.NODE_ENV === 'development',
 	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		})
+
+		return config
+	},
 })
