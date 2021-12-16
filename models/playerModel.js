f4e9fdc271f8f2a66e749e6df8c2ea40653992de
Holmes-EH <holmes.samuel@yahoo.fr>
2021-12-16 19:50:27 +0100

    hash password pre save

--- models/playerModel.js
+++ models/playerModel.js
@@ -22,5 +22,13 @@ playerSchema.methods.matchPassword = async function (enteredPassword) {
 	return await bcrypt.compare(enteredPassword, this.password)
 }
 
+playerSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		next()
+	}
+	const salt = await bcrypt.genSalt(10)
+	this.password = await bcrypt.hash(this.password, salt)
+})
+
 module.exports =
 	mongoose.models.Player || mongoose.model('Player', playerSchema)
