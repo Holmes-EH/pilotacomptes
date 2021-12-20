102d7b50ccdc94b7b12654c36794c0796adb4b3a
Holmes-EH <holmes.samuel@yahoo.fr>
2021-12-20 09:53:59 +0100

    add pwa meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,22 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<meta name='application-name' content='Pilotacomptes' />
+					<meta name='apple-mobile-web-app-capable' content='yes' />
+					<meta
+						name='apple-mobile-web-app-status-bar-style'
+						content='default'
+					/>
+					<meta
+						name='apple-mobile-web-app-title'
+						content='Pilotacomptes'
+					/>
+					<meta
+						name='description'
+						content='Une app pour suivre les comptes pour le créneau de samedi / 11h'
+					/>
+					<meta name='format-detection' content='telephone=no' />
+					<meta name='mobile-web-app-capable' content='yes' />
 					<link rel='manifest' href='/manifest.json' />
 					<link
 						rel='apple-touch-icon'
