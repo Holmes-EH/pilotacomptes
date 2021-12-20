d0e1243a470e4e0d512f9be840738c7379e8ce0c
Holmes-EH <holmes.samuel@yahoo.fr>
2021-12-20 09:53:31 +0100

    set viewport  meta in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+import Head from 'next/head'
 import { AppWrapper } from '@/context/store'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<AppWrapper>
+			<Head>
+				<meta
+					name='viewport'
+					content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover'
+				/>
+			</Head>
 			<Component {...pageProps} />
 		</AppWrapper>
 	)
